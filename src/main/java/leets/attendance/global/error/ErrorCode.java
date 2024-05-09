90a64d8f18e7fb9f496be93a9916ec4853f0a40c
rootTiket <l20078829@gmail.com>
2024-05-10 00:23:48 +0900

    [feat]: user 엔티티 생성, 초기설정

---
+++ src/main/java/leets/attendance/global/error/ErrorCode.java
@@ -0,0 +1,19 @@
+package leets.attendance.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다."),
+    INVALID_TOKEN(401, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다."),
+    INCORRECT_ID(404, "INCORRECT_ID", "올바르지 않은 아이디입니다."),
+    INCORRECT_PASSWORD(404, "INCORRECT_PASSWORD", "올바르지 않은 비밀번호입니다."),
+    USER_CONFLICT(409, "USER_CONFLICT", "이미 가입된 계정입니다.");
+
+    private final int httpStatus;
+    private final String code;
+    private final String message;
+}
---
+++ src/main/java/leets/attendance/global/error/ServiceException.java
@@ -0,0 +1,4 @@
+package leets.attendance.global.error;
+
+public class ServiceException {
+}

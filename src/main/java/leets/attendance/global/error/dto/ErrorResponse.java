208e1cfaf987e124978cb9b4d4503a6ac6eb8ce5
jiixon <jr0107380@gmail.com>
2024-05-09 17:12:27 +0900

    feat: 기본적인 에러코드 구현

--- src/main/java/leets/attendance/global/error/dto/ErrorResponse.java
+++ src/main/java/leets/attendance/global/error/dto/ErrorResponse.java
@@ -1,2 +1,18 @@
-package leets.attendance.global.error.dto;public class ErrorResponse {
+package leets.attendance.global.error.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+    private final int status;
+    private final String error;
+    private final String code;
+    private final String message;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.status = errorCode.getHttpStatus().value();
+        this.error = errorCode.getHttpStatus().name();
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
 }

7a7aeb9aabde02b21a6d16b8f60adbcfb608a7e2
hyxklee <ewgt1234@naver.com>
2024-05-10 16:06:52 +0900

    feat(UserDTO): 유저DTO 구현

--- src/main/java/leets/attendance/dto/UserDTO.java
+++ src/main/java/leets/attendance/dto/UserDTO.java
@@ -1,4 +1,25 @@
 package leets.attendance.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDTO {
+
+    @NotBlank(message = "ID는 필수 입니다")
+    private String userid;
+
+    @NotBlank(message = "비밀번호는 필수 입니다")
+    private String password;
+
+    @NotBlank(message = "비밀번호 확인은 필수 입니다")
+    private String confirmPassword;
+
+    @NotBlank(message = "이름은 필수 입니다")
+    private String name;
+
+    @NotBlank(message = "파트명은 필수 입니다")
+    private String partName;
 }

17f70c09e0314e99963b86a80f962e1115906ebb
hyxklee <ewgt1234@naver.com>
2024-05-10 16:08:46 +0900

    feat(SecurityConfig): 보안 설정 (비밀번호 암호화 빈 등록, 인가 정보 등록)

--- src/main/java/leets/attendance/config/SecurityConfig.java
+++ src/main/java/leets/attendance/config/SecurityConfig.java
@@ -1,4 +1,40 @@
 package leets.attendance.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests((auth)-> auth
+                        .requestMatchers("/login", "/","/join",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**").permitAll()
+                        .requestMatchers("/swagger/**").permitAll()
+                        .anyRequest().authenticated())
+
+                .sessionManagement(session->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
 }

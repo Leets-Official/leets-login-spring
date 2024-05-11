9eec837bc77ca21b9d55069404b023cccacbd5ca
hyxklee <ewgt1234@naver.com>
2024-05-11 17:23:06 +0900

    feat(SecurityConfig): JWT 관련 설정 추가 및 필터 추가

--- src/main/java/leets/attendance/config/SecurityConfig.java
+++ src/main/java/leets/attendance/config/SecurityConfig.java
@@ -1,16 +1,32 @@
 package leets.attendance.config;
 
+import leets.attendance.filter.CustomLoginFilter;
+import leets.attendance.filter.JWTLoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -19,13 +35,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        CustomLoginFilter customLoginFilter = new CustomLoginFilter(authenticationManager(authenticationConfiguration), jwtUtil);
+        customLoginFilter.setFilterProcessesUrl("/users/login"); //스프링 시큐리티의 기본 로그인 경로를 필터 프로세스 경로 변경으로 바꿈
+
         http
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
 
                 .authorizeHttpRequests((auth)-> auth
-                        .requestMatchers("/users/login", "/","/users/register",
+                        .requestMatchers("/users/login", "/users/register",
                                 "/v3/api-docs",
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html",
@@ -33,6 +52,8 @@ public class SecurityConfig {
                         .requestMatchers("/swagger/**").permitAll()
                         .anyRequest().authenticated())
 
+                .addFilterBefore(new JWTLoginFilter(jwtUtil), CustomLoginFilter.class)
+                .addFilterAt(customLoginFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(session->session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();

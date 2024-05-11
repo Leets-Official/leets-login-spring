6f59898dc9f651e806dbe35e1d3e09ace489c3dc
jiixon <jr0107380@gmail.com>
2024-05-11 20:52:44 +0900

    refactor: 에어코드, 응답 수정

--- src/main/java/leets/attendance/global/error/dto/ErrorResponse.java
+++ src/main/java/leets/attendance/global/error/dto/ErrorResponse.java
@@ -1,18 +1,22 @@
 package leets.attendance.global.error.dto;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class ErrorResponse {
-    private final int status;
+    private final HttpStatus status;
     private final String error;
     private final String code;
     private final String message;
 
     public ErrorResponse(ErrorCode errorCode) {
-        this.status = errorCode.getHttpStatus().value();
+        this.status = errorCode.getHttpStatus();
         this.error = errorCode.getHttpStatus().name();
         this.code = errorCode.getCode();
         this.message = errorCode.getMessage();
+    }
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode);
     }
 }

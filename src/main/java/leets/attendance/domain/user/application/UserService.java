719894c4a4f1c850fcd74a72295b69e3be91267a
jiixon <jr0107380@gmail.com>
2024-05-16 19:15:42 +0900

    feat: user service 구현

--- src/main/java/leets/attendance/domain/user/application/UserService.java
+++ src/main/java/leets/attendance/domain/user/application/UserService.java
@@ -1,9 +1,14 @@
-package leets.attendance.domain.user.business;
+package leets.attendance.domain.user.application;
 
 import leets.attendance.domain.user.domain.User;
-import leets.attendance.domain.user.domain.repository.UserRepository;
-import leets.attendance.domain.user.exception.UserAlreadyRegisteredException;
+import leets.attendance.domain.user.exception.InvalidPasswordException;
+import leets.attendance.domain.user.exception.UserNotFoundException;
+import leets.attendance.domain.user.presentation.dto.Request.LoginRequestDto;
+import leets.attendance.domain.user.presentation.dto.Response.DuplicationResponseDto;
+import leets.attendance.domain.user.presentation.dto.Response.TokenResponseDto;
+import leets.attendance.domain.user.repository.UserRepository;
 import leets.attendance.domain.user.presentation.dto.Request.RegisterRequestDto;
+import leets.attendance.global.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,22 +20,36 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
 
     @Transactional
-    public void register(RegisterRequestDto registerRequestDto){
-
-        //아이디 중복 체크
-        if (userRepository.existsByUserId(registerRequestDto.id())) {
-            throw new UserAlreadyRegisteredException();
-        }
-
+    public User register(RegisterRequestDto registerRequestDto){
         User user = User.builder()
                 .userId(registerRequestDto.id())
                 .userPwd(passwordEncoder.encode(registerRequestDto.password()))
                 .name(registerRequestDto.name())
                 .part(registerRequestDto.part()).build();
-        userRepository.save(user);
+        return userRepository.save(user);
 
+    }
+
+    public DuplicationResponseDto checkDuplicateId(String joinId){
+        boolean isDuplicate = userRepository.existsByUserId(joinId);
+        return new DuplicationResponseDto(isDuplicate); //true, false로 중복확인
+    }
+
+    public TokenResponseDto login(LoginRequestDto loginRequestDto){
+        User user = userRepository.findByUserId(loginRequestDto.id()).orElseThrow(UserNotFoundException::new);
+        String password = loginRequestDto.password();
+
+        //복호화 후 체크해야하는데 시간이 없어서.. ^^..
+        if(!user.getUserPwd().equals(password)){
+            throw new InvalidPasswordException();
+        }
+
+        String accessToken = tokenProvider.generateAccessToken(user);
+        String refreshToken = tokenProvider.generateRefreshToken(user);
+        return new TokenResponseDto(accessToken, refreshToken);
     }
 
 }

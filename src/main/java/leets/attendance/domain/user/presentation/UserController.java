d46535de9d67a76bf8fd45fb844f7e125e1147c9
jiixon <jr0107380@gmail.com>
2024-05-16 19:15:57 +0900

    feat: user controller 구현

--- src/main/java/leets/attendance/domain/user/presentation/UserController.java
+++ src/main/java/leets/attendance/domain/user/presentation/UserController.java
@@ -1,2 +1,37 @@
-package leets.attendance.domain.user.presentation.dto;public class UserController {
+package leets.attendance.domain.user.presentation;
+
+import leets.attendance.domain.common.dto.ResponseDto;
+import leets.attendance.domain.user.application.UserService;
+import leets.attendance.domain.user.domain.User;
+import leets.attendance.domain.user.presentation.dto.Request.LoginRequestDto;
+import leets.attendance.domain.user.presentation.dto.Request.RegisterRequestDto;
+import leets.attendance.domain.user.presentation.dto.Response.DuplicationResponseDto;
+import leets.attendance.domain.user.presentation.dto.Response.TokenResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequestDto registerRequestDto){
+        User register = userService.register(registerRequestDto);
+        return ResponseDto.created(URI.create("/users/" + register.getId()));
+    }
+
+    @GetMapping("/duplication/{joinId}")
+    public ResponseEntity<DuplicationResponseDto> checkDuplicateId(@PathVariable String joinId){
+        return ResponseDto.ok(userService.checkDuplicateId(joinId));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        return ResponseDto.ok(userService.login(loginRequestDto));
+    }
+
 }

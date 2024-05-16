417fbea62da9fdd286b577175ebf0a7de6cc64c5
jiixon <jr0107380@gmail.com>
2024-05-16 19:15:31 +0900

    feat: user repository 구현

--- src/main/java/leets/attendance/domain/user/repository/UserRepository.java
+++ src/main/java/leets/attendance/domain/user/repository/UserRepository.java
@@ -1,8 +1,13 @@
-package leets.attendance.domain.user.domain.repository;
+package leets.attendance.domain.user.repository;
 
 import leets.attendance.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByUserId(Long userId);
+    boolean existsByUserId(String userId);
+
+    Optional<User> findByUserId(String userId);
 }

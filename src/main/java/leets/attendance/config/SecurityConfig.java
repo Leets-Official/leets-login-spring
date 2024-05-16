8e5b642d316d5eb8ed74d5879ba63ae51adc9654
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-05-16 13:44:48 +0900

    refactor: 보안 설정 변경

--- src/main/java/leets/attendance/config/SecurityConfig.java
+++ src/main/java/leets/attendance/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import leets.attendance.util.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,35 +28,34 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // CSRF 설정 Disable
         http
-                .csrf(AbstractHttpConfigurer::disable);
+                .csrf(AbstractHttpConfigurer::disable)
 
         //CORS 설정 Disable
-        http
-                .cors(AbstractHttpConfigurer::disable);
+                .cors(AbstractHttpConfigurer::disable)
 
         //From 로그인 방식 disable
-        http
-                .formLogin(AbstractHttpConfigurer::disable);
+                .formLogin(AbstractHttpConfigurer::disable)
 
         //http basic 인증 방식 disable
-        http
-                .httpBasic(AbstractHttpConfigurer::disable);
+                .httpBasic(AbstractHttpConfigurer::disable)
 
         // 시큐리티는 기본적으로 세션을 사용
         // JWT는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
-        http
+
                 .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
         // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
         //경로별 인가 작업
-        http
+
                 .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/users/**", "/attendances/**").permitAll()
-                        .anyRequest().authenticated());
+                        .anyRequest().permitAll()
+                )
+
 
         // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
-        http
                 .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();

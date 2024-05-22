0e25906ddad69b8b9f6eb68b8007371e8ac45b21
unknown <leejh5838@naver.com>
2024-05-22 13:55:41 +0900

    도메인, 서비스 구현

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,21 @@
 package service;
+import domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }

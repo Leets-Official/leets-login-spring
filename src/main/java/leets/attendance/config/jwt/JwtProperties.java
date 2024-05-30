16427ac10ec05fabf8ef1fd68c50ed6b611176f4
jj <syeonie0526@gmail.com>
2024-05-30 15:20:16 +0900

    fix : JwtProperties의 issuer와 secretKey에 값 가져와서 주입

--- src/main/java/leets/attendance/config/jwt/JwtProperties.java
+++ src/main/java/leets/attendance/config/jwt/JwtProperties.java
@@ -2,6 +2,7 @@ package leets.attendance.config.jwt;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("jwt")
 public class JwtProperties {
 
+    @Value("${jwt.issuer}")
     private String issuer;
+    @Value("${jwt.secret_key}")
     private String secretKey;
 }
